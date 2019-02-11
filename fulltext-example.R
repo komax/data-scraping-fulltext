9b72add3935e9798be7b56fbd11cd9ff523415b9
Maximilian Konzack <maximilian.konzack@idiv.de>
2019-02-11 11:49:26 +0100

    R script to search in multiple journals at a time

---
+++ fulltext-example.R
@@ -0,0 +1,25 @@
+
+# Use package fulltext to search and obtain articles from various resources.
+
+# Install the package if needed.
+#install.packages(fulltext)
+
+# Then load the package.
+libary(fulltext)
+
+# Search in these journals and store the dois in a dataframe.
+searchResults <- ft_search(query = "ecology AND biology",
+                           from = c("plos", "biorxiv"))
+
+# Focus on the dois from plos.
+(plosDios <- searchResults$plos$data$id)
+
+# Download the abstracts from these articles.
+abstractsPlosArticles <- ft_abstract(plosDios, from = "plos")
+
+# Output the abstract of the first article
+firstArticle <- abstractsPlosArticles$plos[[1]]
+print(firstArticle$doi)
+print(firstArticle$abstract)
+
+
